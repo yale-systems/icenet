b905e7e26d4c5c47ee269f4f54a1edac6333f1a9
Tanushree Burman <tanushree.burman@yale.edu>
2021-04-30 15:52:24 +0000

    String encoder in progress

--- src/main/scala/Serdes.scala
+++ src/main/scala/Serdes.scala
@@ -167,3 +167,74 @@ class UINTDecoder extends Module
   io.out.bits.data := groups.asUInt
   */
 }
+
+class StringEncode extends Module {
+  val io = IO(new Bundle {
+    val in = Flipped(Decoupled(new StreamChannel(64)))
+    val out = Decoupled(new StreamChannel(80))
+  })
+
+  io.in.valid <> io.out.valid
+  io.in.ready <> io.out.ready
+  io.in.bits.last <> io.out.bits.last 
+  io.in.bits.keep <> io.out.bits.keep 
+
+  //binary representation of wire_type 
+  //binary representation of field_num 
+  val wire_type = 2.U
+  val field_num = 1.U
+  val length = 7.U
+  
+  //two bytes added, one for field num and wire type 
+  //second byte for length
+  val num_groups = 10 
+  val final_groups = Wire(Vec(num_groups, UInt(8.W)))
+  
+  //first byte: (field_num << 3 || wire_type)
+  final_groups(0) := field_num << 3 || wire_type 
+  
+  //**bits coming in are not 64 bits **
+
+  // split 64-bit input data into groups of 7-bits
+  // NOTE: order is reversed (e.g. groups(9) gets io.in(6,0))
+  //val num_groups = 10 // ceil(64/7)
+  val groups = Wire(Vec(num_groups, UInt(7.W)))
+  for (i <- 0 until (num_groups - 1)) {
+    groups(num_groups - i - 1) := io.in.bits.data(7*i+6, 7*i)
+  }
+  groups(0) := Cat(0.U(6.W), io.in.bits.data(63))
+
+  // compute which groups have bits set
+  val has_bits_set = Wire(Vec(num_groups, Bool()))
+  has_bits_set := groups.map( _.orR )
+
+  // Use priority encoder to find the most significant group with a bit set
+  // NOTE: the order of groups is reversed from the original input data, so
+  // the most significant group is actually the smallest index.
+  val most_sig_grp = Wire(UInt(8.W))
+  most_sig_grp := PriorityEncoder(has_bits_set.asUInt)
+
+  // Append one bit to each group, which is 1 for all groups except group X.
+  // Also, assign the most significant group to the 0th group. We don't care
+  // about any groups b/w 0 and most_sig_grp.
+  val new_groups = Wire(Vec(num_groups, UInt(8.W)))
+  new_groups(0) := Cat(0.U(1.W), groups(most_sig_grp))
+  for (i <- 1 until num_groups) {
+    when (i.U + most_sig_grp < num_groups.U) {
+      new_groups(i) := Cat(1.U(1.W), groups(i.U + most_sig_grp))
+    } .otherwise {
+      new_groups(i) := 0.U
+    }
+  }
+
+  //encode the length of the string 
+  final_groups(1) := new_groups(0)
+  
+  //set the rest of the bits to be the same as the original from stream 
+  for (i <- 2 until num_groups) {
+    new_groups(i) := io.in.bits.data(8*i+7, 8*i)
+  }
+  
+  io.out.bits.data := new_groups.asUInt 
+
+}
